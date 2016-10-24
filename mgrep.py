5cd790cf66e65b4c31d0495b8c8c1aa30c4c1322
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-10-24 09:58:56 +0500

    Added(mgrep): filter combos by blacklist, regex and words len

--- mgrep.py
+++ mgrep.py
@@ -1,8 +1,16 @@
 # coding: utf-8
 import sys
+from re import match, UNICODE
 from pymorphy2 import MorphAnalyzer
 
 morpher = MorphAnalyzer()
+
+
+BLACKLIST_WORDS = [u'какой', u'такой', u'этот', u'тот',
+                   u'каждый', u'такие', u'это', u'эта', u'их', u'все']
+BLACKLIST_COMBOS = [
+    [u'доброе', u'утро']
+]
 
 
 def choose_suitable_form(parsed, grammem):
@@ -11,7 +19,17 @@ def choose_suitable_form(parsed, grammem):
             return word.word.capitalize()
 
 
+def is_blacklisted(words):
+    return words in BLACKLIST_COMBOS or any(word in BLACKLIST_WORDS for word in words)
+
+
 def validate(words):
+    if not all(match(u'^[а-я]+$', word, flags=UNICODE) for word in words):
+        return
+    if is_blacklisted(words):
+        return
+    if not all(len(word) > 1 for word in words):
+        return
     parsed = (
         choose_suitable_form(morpher.parse(words[0]), u'ADJF'),
         choose_suitable_form(morpher.parse(words[1]), u'NOUN'),
