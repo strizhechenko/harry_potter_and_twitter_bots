43b409b359fafa8a72d9b5a56bb9ab61efe8cdd3
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-10-26 10:42:04 +0500

    Refactoring(bot): used twitterbot_farm/redis instead of twitterbot_utils/env

--- bot.py
+++ bot.py
@@ -1,63 +1,24 @@
 #!/usr/bin/env python
 
-# coding: utf-8
-
-# pylint: disable=C0111,C0103
-
 from os import getenv
-from twitterbot_utils import Twibot, get_maximum_tweets
-from dictator import Dictator
+from twitterbot_farm import Writer
 from mgrep import process_line
 
-__author__ = "@strizhechenko"
 
-TEMPLATE = unicode(getenv('template', u''), 'utf-8')
+class HarryPotter(Writer):
 
+    def __init__(self, username, host='127.0.0.1'):
+        Writer.__init__(self, username, host)
 
-class Tweets(object):
-
-    def __init__(self, source='net', filename='tweets.txt', net_count=100, redis_db=1):
-        self.reader = None
-        self.tweets = []
-        self.filename = filename
-
-        if source == 'local':
-            self.read()
-            return
-
-        if source == 'redis':
-            self.redis = dictator(redis_db)
-            return
-
-        if source == 'net':
-            self.reader = Twibot(username=getenv('reader_name'))
-            if net_count >= 200:
-                self.net_max()
-            else:
-                self.net(net_count)
-
-    def write(self):
-        tweets = [t.text.encode('utf-8') for t in self.tweets]
-        with open(self.filename, 'w') as f:
-            tweets.append('')
-            f.write("\n".join(tweets))
-
-    def read(self):
-        with open(self.filename) as f:
-            self.tweets = (unicode(tweet, 'utf-8') for tweet in f.readlines())
-
-    def net(self, count):
-        self.tweets = [t.text for t in self.reader.api.home_timeline(count=count)]
-
-    def net_max(self):
-        self.tweets = get_maximum_tweets(self.reader.api.home_timeline, logging=True)
-
-    def process(self):
-        for tweet in self.tweets:
-            result = process_line(tweet)
+    def run(self):
+        self.connection['__last_tweet_id__'] = 1
+        for key, text in self.unprocessed_lines():
+            result = process_line(unicode(text, 'utf-8'))
             if result:
-                print TEMPLATE.format(result).encode('utf-8')
-
+                tweet = unicode(self.template, 'utf-8').format(result).encode('utf-8')
+                print key, tweet
+                self.twibot.api.update_status(tweet)
+                self.connection['__last_tweet_id__'] = key
 
 if __name__ == '__main__':
-    Tweets('net', net_count=1000).process()
+    HarryPotter(getenv('username'), getenv('host', '127.0.0.1')).run()
