4d41b330a6f506acac9182b9b8d2a8cdc2fec6e8
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-10-26 23:45:40 +0500

    Added(bot): duplicate avoid system + extended words blacklist

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+""" 'Harry Potter and Shit' bot """
 from os import getenv
 from twitterbot_farm import Writer
 from mgrep import process_line
+from tweepy.error import TweepError
 
 
 class HarryPotter(Writer):
@@ -12,13 +14,22 @@ class HarryPotter(Writer):
 
     def run(self):
         self.connection['__last_tweet_id__'] = 1
-        for key, text in self.unprocessed_lines():
-            result = process_line(unicode(text, 'utf-8'))
-            if result:
-                tweet = unicode(self.template, 'utf-8').format(result).encode('utf-8')
-                print key, tweet
-                self.twibot.api.update_status(tweet)
+        text_input = dict(self.unprocessed_lines())
+        for key in sorted(text_input, key=int):
+            result = process_line(unicode(text_input[str(key)], 'utf-8'))
+            if not result or self.connection.get(result):
+                print 'skip', result
+                continue
+            tweet = unicode(self.template, 'utf-8').format(result).encode('utf-8')
+            try:
+                tweet_rc = self.twibot.api.update_status(tweet)
+                # pylint: disable=E1101
+                self.connection[result] = tweet_rc.id
+                print 'ok', key, tweet
+                break
+            except TweepError:
                 self.connection['__last_tweet_id__'] = key
+                print 'fail', key, tweet
 
 if __name__ == '__main__':
     HarryPotter(getenv('username'), getenv('host', '127.0.0.1')).run()
