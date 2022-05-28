888abf1a5529fabbea2276cfea47e97f3689636f
Oleg Strizhechenko <oleg@carbonsoft.ru>
2022-05-29 00:57:34 +0500

    Nice

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
-""" TODO: опишите, что это за скрипт """
+#!/usr/bin/env python3
+""" Гарри Поттер и Парсинг lor.sh """
 # coding: utf-8
 
 import argparse
+import logging
 import sqlite3
+
 import html2text
 import requests
 
@@ -11,27 +13,55 @@ from harry_potter_and_twitter_bots.mgrep import pick_combos
 
 h = html2text.HTML2Text()
 h.ignore_links = h.ignore_images = True
+logging.basicConfig(level=logging.INFO)
 
 
 def parse_args():
     """ Разбор аргументов"""
     parser = argparse.ArgumentParser()
-    parser.description = "Опишите, что делает утилита"
+    parser.description = "Вытянуть последние N записей из мастодон в свой SQLite для фразочек бота"
     parser.add_argument('--url', default='https://lor.sh/api/v1/timelines/public?local=true', type=str, help='API URL')
-    parser.add_argument('--limit', default=20, type=int, help='Limit')
+    parser.add_argument('--db', default='harry_potter_and_twitter_bots/mbot.sqlite', type=str, help='Cache DB')
+    parser.add_argument('--limit', default=40, type=int, help='Limit')
     args = parser.parse_args()
     args.url = f'{args.url}&limit={args.limit}'
     return args
+
+
+def toots2toots(cur):
+    for _id, content in cur.execute("SELECT id, content FROM toot WHERE processed = 0").fetchall():
+        for combo in pick_combos(content):
+            print(f"Гарри Поттер и {combo}")
+            count = cur.execute(f"SELECT count(1) FROM phrase WHERE phrase='{combo}'").fetchone()[0]
+            if count == 0:
+                cur.execute(f"INSERT INTO phrase (phrase) VALUES ('{combo}')")
+        cur.execute(f"UPDATE toot SET processed = 1 where id = {_id}")
+
+
+def update_toots(url, cur, html):
+    max_id = cur.execute("SELECT max(id) FROM toot").fetchone()[0]
+    if max_id:
+        url = f'{url}&since_id={max_id}'
+    data = requests.get(url).json()
+    logging.debug("Fetched %d items", len(data))
+    for toot in data:
+        content = html.handle(toot.get('content')).replace("'", "").strip()
+        count = cur.execute(f"SELECT count(1) FROM toot WHERE id={toot.get('id')}").fetchone()[0]
+        if count == 0:
+            sql = f"INSERT INTO toot (id, content) VALUES ({toot.get('id')}, '{content}')"
+            cur.execute(sql)
 
 
 def main():
     args = parse_args()
     html = html2text.HTML2Text()
     html.ignore_links = html.ignore_images = True
-    for toot in requests.get(args.url).json():
-        import json; print(json.dumps(toot, indent=2, ensure_ascii=False)); exit(1)
-        for combo in pick_combos(html.handle(toot.get('content'))):
-            print(f"Гарри Поттер и {combo}")
+    conn = sqlite3.connect(args.db)
+    cur = conn.cursor()
+    update_toots(args.url, cur, html)
+    toots2toots(cur)
+    conn.commit()
+    conn.close()
 
 
 if __name__ == '__main__':
