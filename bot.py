78972f4367c74493cd5fb791a3477eacd0f235f5
Oleg Strizhechenko <oleg@carbonsoft.ru>
2022-05-30 16:28:14 +0500

    Исправлено: поддержка мастодона вместо твиттера.

--- bot.py
+++ bot.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python
 
 """ 'Harry Potter and Shit' bot """
+import sqlite3
 from os import getenv
-from twitterbot_farm import Writer
-from mgrep import process_tweet
-from tweepy.error import TweepError
+
+from mastodon import Mastodon
 
 
-class HarryPotter(Writer):
-
-    def __init__(self, username, host='127.0.0.1'):
-        Writer.__init__(self, username, host)
+class HarryPotter:
+    def __init__(self):
+        params = {key: getenv(key) for key in 'client_id', 'client_secret', 'access_token', 'api_base_url'}
+        self.client = Mastodon(**params)
+        self.db = getenv('db')
 
     def run(self):
-        self.connection['__last_tweet_id__'] = self.connection.get('__last_tweet_id__', '1')
-        text_input = dict(self.unprocessed_lines())
-        for key in sorted(text_input, key=int):
-            result = process_tweet(unicode(text_input[str(key)], 'utf-8'))
-            if not result or self.connection.get(result):
-                continue
-            try:
-                self.tweet(key, result)
-                break
-            except TweepError:
-                self.connection['__last_tweet_id__'] = key
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+        phrase = cur.execute("SELECT phrase FROM phrase WHERE posted = 0 ORDER BY toot_id LIMIT 1").fetchone()[0]
+        self.client.status_post(phrase, visibility='unlisted')
+        cur.execute(f"UPDATE phrase SET posted = 1 WHERE phrase = '{phrase}'")
+        conn.commit()
+        conn.close()
 
 
 if __name__ == '__main__':
-    HarryPotter(getenv('username'), getenv('host', '127.0.0.1')).run()
+    HarryPotter().run()
