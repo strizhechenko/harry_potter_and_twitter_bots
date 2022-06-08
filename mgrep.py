e6a6cd84c99e5043500cb743a80f7ffda9ab194e
Oleg Strizhechenko <oleg@carbonsoft.ru>
2022-06-08 15:01:02 +0500

    Расширена документация

--- mgrep.py
+++ mgrep.py
@@ -13,8 +13,18 @@ IGNORE_PHRASES = [['доброе', 'утро']]
 
 
 def morph(parsed: list, grammems: set, forms: set) -> str or None:
+    """
+    Ищем в распарсенном слове наиболее подходящий под все требования вариант:
+    :param parsed: анализруемое слово парсится в список наиболее вероятных результатов парсинга
+    :param grammems: множество граммем: падеж, число и опционально пол.
+    :param forms : обычно просто часть речи (существительное/прилагательное/причастие), но для причастия нужен залог.
+        Это множество используется дважды:
+        1. При отбросе неправильных вариантов парсинга слова
+        2. При выборе подходящей по всем параметрам лексемы.
+    :return: либо _идеальное_ слово с большой буквы, либо ничего.
+    """
     for word in parsed:
-        if any(form in word.tag for form in forms) and word.score > 0.1:
+        if all(form in word.tag for form in forms) and word.score > 0.1:  # < 0.1 - высосано из пальца.
             for lexeme in word.lexeme:
                 if lexeme.normal_form in IGNORE_WORDS:
                     return
@@ -24,6 +34,13 @@ def morph(parsed: list, grammems: set, forms: set) -> str or None:
 
 
 def add_common_tag(words: list, category: tuple, tags: set) -> bool or None:
+    """
+    Ищем общий тег в заданной категории (множестве тегов) в двух словах.
+    :param words: собственно, распарсенные слова.
+    :param category: множество допустимых общих тегов (ТОТ САМЫЙ СПИСОК ВСЕХ ГЕНДЕРОВ)
+    :param tags: итоговое множество общих тегов, которое затем будет использовано для фильтрации вариантов слов.
+    :return: True, если тег нашёлся и добавлен в множество общих тегов, None если нет. Эдакий int rc.
+    """
     for tag in category:
         if all(any(tag in w.tag for w in word) for word in words):
             tags.add(tag)
@@ -41,6 +58,10 @@ def detect_adjf_tags(adjf):
 
 
 def morphs(words: tuple) -> tuple:
+    """
+    :param words: входящая пара слов (2 строки)
+    :return: исходящая пара слов (2 строки или None, None)
+    """
     parsed = list(map(morpher.parse, words))
     common_tags = {'nomn'}  # стараемся привести всё к именительному падежу
     # Обеспечиваем согласованность по числу, множественное в приоритете
@@ -58,6 +79,11 @@ def morphs(words: tuple) -> tuple:
 
 
 def pick_combos(line: str):
+    """
+    Точка входа в библиотеку.
+    :param line: просто строка текста. Хоть целую книгу можно сюда засунуть.
+    :return: подходящие под заданный шаблон пары слов
+    """
     words = line.lower().split()
     for n, word in enumerate(words[1:], 1):
         combo = (words[n - 1], word)
